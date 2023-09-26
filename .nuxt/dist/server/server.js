c2fd1cdba0043616ef7c794af2d119d051b249b4
techcoder01 <techcoderhelp@gmail.com>
2023-09-26 11:10:33 +0500

    4th

--- .nuxt/dist/server/server.js
+++ .nuxt/dist/server/server.js
@@ -691,7 +691,7 @@ async function setContext(app, context) {
   // If context not defined, create it
   if (!app.context) {
     app.context = {
-      isStatic: false,
+      isStatic: true,
       isDev: false,
       isHMR: false,
       app,
@@ -1975,6 +1975,9 @@ const layouts = {
     },
     isFetching() {
       return this.nbFetching > 0;
+    },
+    isPreview() {
+      return Boolean(this.$options.previewData);
     }
   },
   methods: {
