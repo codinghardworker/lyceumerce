5ca05cc5d4b339647253500eacee86ad0351f07c
techcoder01 <techcoderhelp@gmail.com>
2023-09-26 15:43:22 +0500

    Lyceumerce final

--- nuxt.config.js
+++ nuxt.config.js
@@ -66,6 +66,7 @@ export default {
   // target: "static",
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [],
+  devServerHandlers: [],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [["vue-scrollto/nuxt", { duration: 1000 }]],
